6a1e121dccef8f23a355939e34ae8d38a26ae571
Dexius <dexxx@example.com>
2026-05-26 17:14:39 +0300

    added new tests def test_edit_user_with_invalid_data

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -141,7 +141,6 @@ class TestUserEdit(BaseCase):
             headers={"x-csrf-token" : token}, 
             cookies={"auth_sid":auth_sid},
             json={"firstName":new_name})
-        print(response4.text)
         
         Assertions.assert_code_status(response4, 400) 
         
@@ -152,6 +151,56 @@ class TestUserEdit(BaseCase):
             "Expected error message when editing another user's data"
         )
 
+    @pytest.mark.parametrize(
+        "field, invalid_value, expected_error", 
+    [
+    ("email", "invalidemail.com", "Invalid email format"),
+    ("firstName", "A", "The value for field `firstName` is too short")
+    ],
+    ids =['wrongmail', 'oneletterName'] 
+    )
+    def test_edit_user_with_invalid_data(self, field, invalid_value, expected_error):
+        
+        #register
+        register_data = self.prepare_reg_user_data()
+        response1 = requests.post(url_get, json=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data['email']
+        firstName = register_data['firstName']
+        password = register_data['password']
+        user_id = self.get_json_value(response1, "id")
+
+        #login
+        login_data = {
+            'email' : email,
+            'password' : password
+        }
+
+        response2 = requests.post(url_login, json=login_data)
+
+        Assertions.assert_code_status(response2, 200)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        #edit
+        response3 = requests.put(
+            f"https://playground.learnqa.ru/api/user/{user_id}", 
+            headers={"x-csrf-token" : token}, 
+            cookies={"auth_sid":auth_sid},
+            json={field: invalid_value})
+
+        Assertions.assert_code_status(response3, 400)
+        
+        Assertions.assert_json_value_by_name(
+            response3,
+            "error",
+            expected_error,
+            "Expected error message when editing another user's data"
+        )
 
     
 
